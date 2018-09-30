faf89482a3f9392e6cfffb5788a03d7e32e8455f
hxor <hakimsembiring@outlook.co.id>
2018-09-30 14:34:20 +0700

    Make Server Side Validation and Storing Data into Database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -37,7 +37,13 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email'
+        ]);
+
+        $model = User::create($request->all());
+        return $model;
     }
 
     /**
--- public/js/app.js
+++ public/js/app.js
@@ -18,3 +18,42 @@ $('body').on('click', '.modal-show', function (event) {
 
     $('#modal').modal('show');
 });
+
+$('#modal-btn-save').click(function (event) {
+    event.preventDefault();
+
+    var form = $('#modal-body form'),
+        url = form.attr('action'),
+        method = $('input[name=_method]').val() == undefined ? 'POST' : 'PUT';
+
+    form.find('.help-block').remove();
+    form.find('.form-group').removeClass('has-error');
+
+    $.ajax({
+        url : url,
+        method: method,
+        data : form.serialize(),
+        success: function (response) {
+            form.trigger('reset');
+            $('#modal').modal('hide');
+            $('#datatables').DataTable().ajax.reload();
+
+            swal({
+                type : 'success',
+                title : 'Success!',
+                text : 'Data has been saved!'
+            });
+        },
+        error : function (xhr) {
+            var res = xhr.responseJSON;
+            if ($.isEmptyObject(res) == false) {
+                $.each(res.errors, function (key, value) {
+                    $('#' + key)
+                        .closest('.form-group')
+                        .addClass('has-error')
+                        .append('<span class="help-block"><strong>' + value + '</strong></span>');
+                });
+            }
+        }
+    })
+});
